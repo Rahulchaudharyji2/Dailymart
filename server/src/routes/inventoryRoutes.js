cfb9066b3ae728b519cc7676b9913031eddc002b
Tanay Kumar <155322101+acode-42@users.noreply.github.com>
2025-07-08 12:54:20 +0530

    Delete server directory

--- server/src/routes/inventoryRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getPredictions, createProduct } = require("../controller/inventoryController");
-
-router.get("/", getPredictions);
-router.post("/add", createProduct);
-
-module.exports = router;
